96f6523eebade23d90271f4d14081eb4ebb0e856
LewisC00 <lewisculleton00@gmail.com>
2021-07-22 23:18:33 +0100

    Major overhaul of landing page and laout

--- js/App.js
+++ js/App.js
@@ -2,3 +2,22 @@
 particlesJS.load('particles-js', 'js/particles-config.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+function load(){
+  console.log("page loaded");
+  $("#site-header").slideDown(3,3, function(){
+    console.log("Bar down")
+  });
+}
+
+var topofDiv = $(".landing").offset().top + $(".landing").outerHeight(true); //gets offset of header
+$(window).scroll(function(){
+    if($(window).scrollTop() > topofDiv){
+       $(".sticky-top").removeClass("site-header-light");
+       $(".sticky-top").addClass("site-header");
+    }
+    else{
+      $(".sticky-top").removeClass("site-header");
+      $(".sticky-top").addClass("site-header-light");
+    }
+});
